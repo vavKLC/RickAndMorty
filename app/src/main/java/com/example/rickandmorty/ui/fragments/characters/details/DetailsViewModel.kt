69220a56b522887857a7ac249d0089ac6148e349
vavKLC <batyrkanov007@gmail.com>
2022-03-23 17:37:02 +0600

    VRODE NORM

---
+++ app/src/main/java/com/example/rickandmorty/ui/fragments/characters/details/DetailsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.rickandmorty.ui.fragments.characters.details
+
+import com.example.rickandmorty.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class DetailsViewModel : BaseViewModel() {
+}
